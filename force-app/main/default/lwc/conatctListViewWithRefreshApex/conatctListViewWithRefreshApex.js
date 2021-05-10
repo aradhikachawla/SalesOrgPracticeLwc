65659cf9828b7bbe1a18387eba11d1d135e9b2aa
Aradhika Chawla <aradhikac@gmail.com>
2021-05-10 07:31:29 -0700

    forth commit

--- force-app/main/default/lwc/conatctListViewWithRefreshApex/conatctListViewWithRefreshApex.js
+++ force-app/main/default/lwc/conatctListViewWithRefreshApex/conatctListViewWithRefreshApex.js
@@ -8,7 +8,7 @@ import { refreshApex } from '@salesforce/apex';
 import { NavigationMixin } from 'lightning/navigation';
 
 ////Pushed to Git
-
+/// Pushed to Git now
 // row actions
 const actions = [
     { label: 'Delete', name: 'delete' }
